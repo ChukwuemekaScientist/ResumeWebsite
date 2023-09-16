f0cdd045bb7cdf981f37ccbabbd742b8917a1ee8
ChukwuemekaScientist <112355364+ChukwuemekaScientist@users.noreply.github.com>
2023-09-16 04:55:04 +0000

    output statement

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.getElementById("outputstatement").innerHTML = 5 + 6;
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('subform');
+    const output = document.getElementById('output');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        
+        const nameInput = document.getElementById('name');
+
+        const name = nameInput.value;
+
+        // Display the values in the output div
+        output.innerHTML = `<p>Hi ${name}, very nice to meet you.</p>`;
+    });
+});
